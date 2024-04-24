369bc08ea324a8b0bf1e395523e388e7ffa22f73
UsmanAzhigov <azigovusman@gmail.com>
2024-04-24 22:55:24 +0300

    add test

--- src/question.ts
+++ src/question.ts
@@ -23,4 +23,34 @@ export const questions = [
     options: ['Hello', 'Goodbye', 'Thanks', 'Yes'],
     correctAnswer: 'Hello',
   },
+  {
+    question: 'What is the opposite of "big"?',
+    options: ['Small', 'Large', 'Huge', 'Tiny'],
+    correctAnswer: 'Small',
+  },
+  {
+    question: 'Choose the correct form of the verb: "She ___ to the store yesterday."',
+    options: ['Go', 'Goes', 'Went', 'Going'],
+    correctAnswer: 'Went',
+  },
+  {
+    question: 'Which sentence is correct?',
+    options: [
+      'He plays tennis every Sundays.',
+      'She have a lot of friends.',
+      "They doesn't like pizza.",
+      'We are going to the cinema tonight.',
+    ],
+    correctAnswer: 'We are going to the cinema tonight.',
+  },
+  {
+    question: 'What is the past tense of "eat"?',
+    options: ['Eat', 'Eaten', 'Ate', 'Eating'],
+    correctAnswer: 'Ate',
+  },
+  {
+    question: 'Choose the correct word: "I want ___ orange juice."',
+    options: ['Some', 'Any', 'Many', 'Much'],
+    correctAnswer: 'Some',
+  },
 ];
